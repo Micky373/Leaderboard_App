4541d34cba9c70340e02ea59a1c4961059c7425d
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-07 09:48:22 +0300

    Finalise the mobile and desktop version

--- src/modules/getScore.js
+++ src/modules/getScore.js
@@ -1,11 +1,11 @@
 import deliverArray from './deliverArray.js';
 
 const getScore = async () => {
-  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5fnWrqyXm0Y5iMiMMw2U/scores/');
-  const dataObj = await response.json();
-  const dataArr = dataObj.result;
-  dataArr.sort((a, b) => b.score - a.score);
-  deliverArray(dataArr);
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5fnWrqyXm0Y5iMiMMw2U/scores/');
+    const dataObj = await response.json();
+    const dataArr = dataObj.result;
+    dataArr.sort((a, b) => b.score - a.score);
+    deliverArray(dataArr);
 };
 
 export default getScore;
